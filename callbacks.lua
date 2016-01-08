be602f37dbbc9c7f09fcee3e26d5a9933a29a7cf
Jared Norman <jared@creepywizard.com>
2016-01-07 23:51:12 -0800

    Add a placeholder draw

--- callbacks.lua
+++ callbacks.lua
@@ -1,4 +1,5 @@
 local white = require('colours').white
+local black = require('colours').black
 
 return function(target)
   target.load = function()
@@ -9,6 +10,9 @@ return function(target)
   end
 
   target.draw = function()
+    love.graphics.scale(2)
+    love.graphics.setColor(black())
+    love.graphics.print("This is working", 0, 0)
   end
 
   target.mousepressed = function(x, y, button)
