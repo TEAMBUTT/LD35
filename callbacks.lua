a590c4c57d9631f973bf9cd869c61cbadd0a6494
Jared Norman <jared@creepywizard.com>
2016-01-07 21:51:58 -0800

    Include a full set of callbacks

--- callbacks.lua
+++ callbacks.lua
@@ -2,4 +2,28 @@ return function(target)
   target.load = function()
     love.graphics.setBackgroundColor(255, 255, 255)
   end
+
+  target.update = function(dt)
+  end
+
+  target.draw = function()
+  end
+
+  target.mousepressed = function(x, y, button)
+  end
+
+  target.mousereleased = function(x, y, button)
+  end
+
+  target.keypressed = function(key)
+  end
+
+  target.keyreleased = function(key)
+  end
+
+  target.focus = function(focused)
+  end
+
+  target.quit = function()
+  end
 end
--- spec/callback_spec.lua
+++ spec/callback_spec.lua
@@ -3,9 +3,17 @@ require 'env'
 local callbacks = require 'callbacks'
 
 describe("LOVE callbacks", function()
-  it("loads something", function()
+  it("loads some callbacks of some kind", function()
     local table = {}
     callbacks(table)
     assert.equal(type(table.load), "function")
+    assert.equal(type(table.update), "function")
+    assert.equal(type(table.draw), "function")
+    assert.equal(type(table.mousepressed), "function")
+    assert.equal(type(table.mousereleased), "function")
+    assert.equal(type(table.keypressed), "function")
+    assert.equal(type(table.keyreleased), "function")
+    assert.equal(type(table.focus), "function")
+    assert.equal(type(table.quit), "function")
   end)
 end)
