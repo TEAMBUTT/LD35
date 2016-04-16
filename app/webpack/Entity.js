6b0232439e555d1fc834a066b510aecbf62ef79c
John Hawthorn <john.hawthorn@gmail.com>
2016-04-16 14:18:16 -0700

    Don't make entities if no actions available

--- app/webpack/Entity.js
+++ app/webpack/Entity.js
@@ -10,12 +10,18 @@ class Entity {
   }
 
   html(text) {
+    const actions = this.actions();
+
+    if(!actions.length) {
+      return $(`<span>${text}</span>`);
+    }
+
     const $element = $(`<div class="entity tooltip">${text}</div>`);
     const $tooltipContainer = $('<div class="tooltip-content"></div>');
 
     $element.append($tooltipContainer);
 
-    each(this.actions(), (action) => {
+    each(actions, (action) => {
       const $action = $(`<div class="action-label">${action.label}</div>`);
 
       $action.on('click', () => {
