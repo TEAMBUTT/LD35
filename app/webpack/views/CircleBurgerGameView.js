3640bcf3f7901d85c77403c72042b5ffce9fa448
Jared Norman <jared@creepywizard.com>
2016-04-16 00:09:12 -0700

    Pull thing out into function

--- app/webpack/views/CircleBurgerGameView.js
+++ app/webpack/views/CircleBurgerGameView.js
@@ -8,9 +8,10 @@ module.exports = class {
   start() {
     this.messageBox = $('<div class="message-box">');
     this.$frame.append(this.messageBox);
-    onMessage((message) => {
-      this.messageBox.append(message);
-    });
+    onMessage((message) => this.displayMessage(message));
     addMessage($('<h1>Wake up! You\'re late for your shift!</h1>'));
+  }
+  displayMessage(message) {
+    this.messageBox.append(message);
   }
 }
