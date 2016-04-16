dcfb2eced6fdaf2b25c9deb7b24d006f69f15f73
Andrew Stewart <andrew@stwrt.ca>
2016-04-16 01:34:26 -0700

    We've got multiline strings, let's use them

--- app/webpack/scenes/bedroom.js
+++ app/webpack/scenes/bedroom.js
@@ -4,7 +4,12 @@ import Scene from '../Scene.js';
 
 class Bedroom extends Scene {
   descriptionMessage() {
-    return $('<p>Your alarm clock is ringing beside you on the bedside table. Your uniform is on the floor beside your mirror.</p>');
+    return $(`
+      <p>
+        Your alarm clock is ringing beside you on the bedside table.
+        Your uniform is on the floor beside your mirror.
+      </p>
+    `);
   }
 }
 
