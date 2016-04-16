69407717206b589c46e741acadb9d0845c025004
John Hawthorn <john.hawthorn@gmail.com>
2016-04-16 13:39:16 -0700

    Allow returning to bedroom

--- app/webpack/scenes/homeHallway.js
+++ app/webpack/scenes/homeHallway.js
@@ -1,14 +1,17 @@
 import $ from 'jquery';
 
 import Scene from '../Scene.js';
+import bedroom from '../scenes/bedroom.js';
+import Door from '../entities/Door.js';
 
 export class HomeHallway extends Scene {
   descriptionMessage() {
+    const bedroomDoor = new Door(bedroom);
     const message = `
-      You are in the hallway outside your bedroom.
+      You are in the hallway outside your <%= entity("bedroomDoor", "bedroom") %>.
     `;
 
-    return this.insertEntities(message, {});
+    return this.insertEntities(message, {bedroomDoor});
   }
 }
 
