133024954b813afd7fcac1bd1cd9cbbe893af1cc
Andrew Stewart <andrew@stwrt.ca>
2016-04-15 23:22:53 -0700

    Choosy Moms Choose Bluebird, Not Deferred
    jQuery sux

--- app/webpack/views/Intro.js
+++ app/webpack/views/Intro.js
@@ -1,5 +1,6 @@
 import $ from 'jquery';
 import _ from 'lodash';
+import Promise from "bluebird";
 
 let template = _.template(`
   <div class="intro-screen">
@@ -10,15 +11,16 @@ let template = _.template(`
 module.exports = class {
   start() {
     const frame = $('#game');
-    const deferred = $.Deferred();
-    frame.html(this.html('The second game by butt.team'));
-    setTimeout(() => frame.html(this.html('Work Shift')), 1000);
-    setTimeout(() => frame.html(this.html("You are a people shaped person.<br/>Are you a bad enough dude to rescue the president?")), 2000);
-    setTimeout(() => {
-      frame.empty();
-      deferred.resolve();
-    }, 5000);
-    return deferred;
+
+    return new Promise((resolve, reject) => {
+      frame.html(this.html('The second game by butt.team'));
+      setTimeout(() => frame.html(this.html('Work Shift')), 1000);
+      setTimeout(() => frame.html(this.html("You are a people shaped person.<br/>Are you a bad enough dude to rescue the president?")), 2000);
+      setTimeout(() => {
+        frame.empty();
+        resolve();
+      }, 2000);
+    });
   }
   html(text) {
     return template({ text });
