9ed4af600d2acabe6323f7c069a949de6b536669
Jared Norman <jared@creepywizard.com>
2016-04-16 14:26:36 -0700

    Add global state

--- app/webpack/entities/alarmClock.js
+++ app/webpack/entities/alarmClock.js
@@ -1,6 +1,7 @@
 import Entity from '../Entity.js';
 import printMessage from '../printMessage.js';
 import action from '../action.js';
+import state from '../state.js';
 
 export class AlarmClock extends Entity {
   constructor() {
@@ -16,6 +17,7 @@ export class AlarmClock extends Entity {
     if(this.ringing) {
       return [
         action("Snooze", () => {
+          state.currentTime += 5;
           printMessage('You sleep for another 5 minutes before the alarm clock rings again.');
         }),
         action("Turn if off.", () => {
---
+++ app/webpack/state.js
@@ -0,0 +1,3 @@
+export default {
+  currentTime: 0
+};
