4cbc41b1c800ecd8eb6588f01380635ef9ba2f01
Jared Norman <jared@creepywizard.com>
2016-04-15 18:24:41 -0700

    Add a quick intro

--- app/webpack/views/Intro.js
+++ app/webpack/views/Intro.js
@@ -1,13 +1,22 @@
 import $ from 'jquery';
+import Handlebars from 'handlebars';
+
+let template = Handlebars.compile(`
+  <div class="intro-screen">
+    <div class="intro-screen-title">{{text}}</div>
+  </div>
+`)
 
 module.exports = class {
-  constructor() {
-    console.log('okay');
-  }
   start() {
     const frame = $('#game');
-    this.deferred = $.Deferred();
-
-    return this.deferred;
+    const deferred = $.Deferred();
+    frame.html(this.html('A game by butt.team'));
+    setTimeout(() => frame.html(this.html('Name of game')), 1000);
+    setTimeout(() => deferred.resolve(), 2000);
+    return deferred;
+  }
+  html(text) {
+    return template({ text });
   }
 };
