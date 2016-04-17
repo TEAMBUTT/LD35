110b6aa6f6b6b87d84f95cada9bec9c4b4b1057f
John Hawthorn <john.hawthorn@gmail.com>
2016-04-17 16:42:03 -0700

    Style report card

--- app/webpack/views/ReportCard.js
+++ app/webpack/views/ReportCard.js
@@ -5,25 +5,25 @@ import state, { results } from "state";
 
 let template = _.template(`
   <div class="report-card-screen">
-    <h1>Report Card</h1>
+    <section class="results">
+    <h1>Employee performance review</h1>
 
-    <% if (results.length) { %>
-      <section class="results">
-        <h3 class="results-total-score">
-          Total score:
-          <%= _.sumBy(results, 'score') %>
-        </h3>
+      <table class="results-table">
+        <% _.each(results, function(result) { %>
+          <tr>
+          <td class="results-score"><%= result.score %></td>
+          <td class="results-description"><%= result.description %></td>
+          </tr>
+        <% }) %>
+      </table>
 
-        <table class="results-table">
-          <% _.each(results, function(result) { %>
-            <tr>
-            <td class="results-score"><%= result.score %></td>
-            <td class="results-description"><%= result.description %></td>
-            </tr>
-          <% }) %>
-        </table>
-      </section>
-    <% } %>
+      <h3 class="results-total-score">
+        Total score:
+        <%= _.sumBy(results, 'score') %>
+      </h3>
+
+      <a class="play-again" href="/">Play again</a>
+    </section>
   </div>
 `)
 
