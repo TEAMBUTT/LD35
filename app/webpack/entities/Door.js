b126158b6da3e3bfece846549e846ce3fddc6335
John Hawthorn <john.hawthorn@gmail.com>
2016-04-16 19:36:40 -0700

    ooooops

--- app/webpack/entities/Door.js
+++ app/webpack/entities/Door.js
@@ -1,4 +1,4 @@
-import Entity, { printMessage, action, time, state } from "../../Entity.js";
+import Entity, { printMessage, action, time, state } from "../Entity.js";
 import { changeScene } from '../currentScene.js';
 
 export default class Door extends Entity {
