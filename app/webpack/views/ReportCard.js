5e50f213c9efc6e813c7818cddd5c66de34ac13a
Adam Mueller <adam.mathys@gmail.com>
2016-04-18 09:44:22 -0700

    DOOM Results

--- app/webpack/views/ReportCard.js
+++ app/webpack/views/ReportCard.js
@@ -8,25 +8,19 @@ let template = _.template(`
   <div class="report-card-screen">
     <section class="results">
       <h1>Employee performance review</h1>
-      <% if (state.doom.progress === 100) { %>
-        <h3 class="results-total-score doom">
-          Total score: 666
-        </h3>
-      <% } else { %>
-        <table class="results-table">
-          <% _.each(results, function(result) { %>
-            <tr>
-            <td class="results-score"><%= result.score %></td>
-            <td class="results-description"><%= result.description %></td>
-            </tr>
-          <% }) %>
-        </table>
+      <table class="results-table">
+        <% _.each(results, function(result) { %>
+          <tr>
+          <td class="results-score"><%= result.score %></td>
+          <td class="results-description"><%= result.description %></td>
+          </tr>
+        <% }) %>
+      </table>
 
-        <h3 class="results-total-score">
-          Total score:
-          <%= _.sumBy(results, 'score') %>
-        </h3>
-      <% } %>
+      <h3 class="results-total-score">
+        Total score:
+        <%= _.sumBy(results, 'score') %>
+      </h3>
 
       <a class="play-again" href="/">Play again</a>
     </section>
@@ -36,6 +30,20 @@ let template = _.template(`
 export default class {
   start() {
     const frame = $('#report');
-    frame.html(template({ results: calculateResults() }));
+    if (state.doom.progress === 100) {
+      $(".main-content").addClass("doom");
+      let doomResults = [
+        { score: 43, description: "Left your alarm ringing. Fight the power!" },
+        { score: 50, description: "Remembered to dress for the occaision." },
+        { score: 79, description: "Didn't eat the granola bar." },
+        { score: 128, description: "Investigated the strange noises. Brave!" },
+        { score: 167, description: "Carved a pentagram in your arm." },
+        { score: 250, description: "Met the mysterious stranger." },
+        { score: -1383, description: "Might have inadvertently started the apocalypse." },
+      ];
+      frame.html(template({ results: doomResults }));
+    } else {
+      frame.html(template({ results: calculateResults() }));
+    }
   }
 };
