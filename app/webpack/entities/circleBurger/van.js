e9113acea462340e6d52f18f851d31b60a455940
John Hawthorn <john.hawthorn@gmail.com>
2016-04-18 10:55:15 -0700

    Van is google analytics

--- app/webpack/entities/circleBurger/van.js
+++ app/webpack/entities/circleBurger/van.js
@@ -11,6 +11,7 @@ export class Van extends Entity {
         "Knock on the window.",
         () => {
           printMessage("You see some movement inside the van, but there's no respones.");
+          printMessage(`On the side you notice the inscription: "Google Analytics"`);
           state.currentTime.add(5, 'minutes');
         }
       )
