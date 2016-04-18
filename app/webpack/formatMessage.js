88cb5d3a2aebec2acfe3069f1d88eeebb2568bfc
John Hawthorn <john.hawthorn@gmail.com>
2016-04-17 22:58:11 -0700

    Only use invoked entities

--- app/webpack/formatMessage.js
+++ app/webpack/formatMessage.js
@@ -4,26 +4,28 @@ import { extend, each } from 'lodash';
 import state from './state.js';
 import { isInInventory } from './inventory.js';
 
-export default function formatMessage(message, entities) {
+export default function formatMessage(message, data) {
+  let entities = [];
   let helpers = {
     entity: (entity, text) => {
-      return entities[entity].placeholder(text)
+      entities.push(entity);
+      return data[entity].placeholder(text);
     },
     state: state,
     isInInventory: isInInventory
   };
 
-  extend(helpers, entities);
+  extend(helpers, data);
 
   message = _.template(message)(helpers);
   message = $('<div class="message scene-description">').append(message);
 
   each(entities, (entity) => {
     message
-      .find(entity.placeholderSelector())
+      .find(data[entity].placeholderSelector())
       .each((index, element) => {
         const $element = $(element);
-        $element.replaceWith(entity.html($element.text()));
+        $element.replaceWith(data[entity].html($element.text()));
       });
   });
   return message;
