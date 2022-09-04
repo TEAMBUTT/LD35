08f1554d8429f1bbd254e15b8d8d73e0cc87937d
John Hawthorn <john@hawthorn.email>
2022-09-04 14:10:52 -0700

    Working (empty) scss

--- webpack.config.js
+++ webpack.config.js
@@ -10,4 +10,22 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              // Prefer `dart-sass`
+              implementation: require("sass"),
+            },
+          },
+        ],
+      },
+    ],
+  },
 };
